2444d01cbb409ca7d9842359491ef241e9cc31be
King-odysseus <wingzatari@gmail.com>
2024-01-18 10:46:52 +0000

    test

--- app.js
+++ app.js
@@ -5,19 +5,19 @@
 function getRandomHex() {
     const hexLetter = '0123456789ABCDEF';
     let color = '#';
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++){
         color += hexLetter[Math.floor(Math.random() * 16)];
     }
-    return color
+    return color;
 }
 // function to update background and text
+function updateHex() {
+    let bgColor = getRandomHex();
+    document.body.style.backgroundColor = bgColor;
+    document.getElementById('bg-text').textContent = `Background Color: ${bgColor}`;
+}
 
-    function updateHex() {
-        let bgColor = getRandomHex();
-        document.body.style.backgroundColor = bgColor
-        document.getElementById('bg-text').textContent = `Background Color: ${bgColor}`;
-    }
 
 // function to update button
     
-    document.getElementById('btn').addEventListener('click', updateHex);
+document.getElementById('btn').addEventListener('click', updateHex);
